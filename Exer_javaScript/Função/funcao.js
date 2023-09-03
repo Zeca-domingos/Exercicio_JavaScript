263bae99c28c5998b03ed53c8b86a3af9f0cff69
Zeca-domingos <ezequielsirino@hotmail.com>
2023-09-03 16:12:49 -0300

    Adicao calcularpreco

--- Exer_javaScript/Função/funcao.js
+++ Exer_javaScript/Função/funcao.js
@@ -1,21 +1,47 @@
 
-
+ //
  function escreverNome (nome){
 
-    console.log('Meu nome é ' + nome);
+    return 'Meu nome é ' + nome;
 
  }
 
- escreverNome('Gabi');
- escreverNome('Santos');
 
  function verificarIdade(idade){
    if(idade >= 18){
-      console.log('Maior');
+      console.log(escreverNome('Fernando') + ' é maior de idade');
    } else {
-       console.log('Menor');
+       console.log(escreverNome('Maria') + ' é menor de idade');
    }
   
  }
  
- verificarIdade(25);
+ verificarIdade(2);
+ // 
+ 
+ function aplicarDesconto(valor, desconto){
+   return (valor - (valor * (desconto / 100)));
+
+ }
+
+ function aplicarJuros(valor, juros){
+   return (valor + (valor * (juros / 100)));
+ }
+
+ const precoEtiqueta =100;
+ const formaDePagamento = 4;
+
+ if(formaDePagamento === 1) {
+    console.log(aplicarDesconto(precoEtiqueta, 10));
+
+ }else if(formaDePagamento === 2) {
+   console.log(aplicarDesconto(precoEtiqueta, 15));
+
+ } else if(formaDePagamento === 3){
+   console .log(precoEtiqueta);
+
+ } else {
+   console.log(aplicarJuros(precoEtiqueta, 10));
+ }
+
+ 
